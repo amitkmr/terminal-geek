ceee5ebf764d5a239d2f589277fefc63d954b9d4
Amit Kumar <amitkmr@users.noreply.github.com>
2016-11-17 23:10:22 +0530

    first version of drive upload and download working fine. :)

--- Terminal Drive/drive.py
+++ Terminal Drive/drive.py
@@ -3,16 +3,18 @@ from __future__ import print_function
 import httplib2
 import os
 import json
+import sys
+from googleapiclient.http import MediaFileUpload
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
+# try:
+#     import argparse
+#     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+# except ImportError:
+#     flags = None
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/drive-python-quickstart.json
@@ -27,20 +29,100 @@ def PrintJSON(dict):
 def UplaodFile(service, file_path):
     """ Upload file to Drive Home folder"""
 
-    print("Uploads files to Google Drive")
+    filename = file_path.split('/').pop()
+    extension = filename.split('.').pop()
+
+    doc_mimeType = {
+        'html':'text/html',
+        'docx':'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'rtf':'application/rtf',
+        'txt':'text/plain',
+        'odt':'application/vnd.oasis.opendocument.text',
+    }
+
+    xls_mimeType = {
+        'csv':'text/csv',
+        'xlsx':'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        'ods':'application/x-vnd.oasis.opendocument.spreadsheet',
+
+    }
+
+    slide_mimeTypes = {
+        'pdf':'application/pdf',
+        'odp':'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+        'pptx': 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+    }
+
+    image_mimeTypes = {
+        'jpeg':'image/jpeg',
+        'png':'image/png',
+        'svg':'image/svg+xml'
+    }
+
+    upload_mimeType = ''
+
+    if extension in doc_mimeType.keys():
+        mimeType = doc_mimeType[extension]
+        upload_mimeType = 'application/vnd.google-apps.document'
+    elif extension in xls_mimeType:
+        mimeType = xls_mimeType[extension]
+        upload_mimeType = 'application/vnd.google-apps.spreadsheet'
+    elif extension in slide_mimeTypes:
+        mimeType = slide_mimeTypes[extension]
+        upload_mimeType = 'application/vnd.google-apps.presentation'
+    elif image_mimeTypes:
+        mimeType = image_mimeTypes[extension]
+        upload_mimeType = image_mimeTypes[extension]
+    else :
+        print ("Unknow Extension Can't uplaod")
+        return
+
+    media = MediaFileUpload(file_path,
+                            mimetype = mimeType,
+                            resumable=True)
+
+    file_metadata = {
+        'name': filename,
+        'mimeType': upload_mimeType
+    }
+
+    file = service.files().create(body=file_metadata,
+                                        media_body=media,
+                                        fields='id').execute()
+
+    print("File Uploaded Successfully..")
+    print('File ID: %s' % file.get('id'))
+
+
+def WriteToFile(filename,file_content):
+
+    current_path = os.getcwd()
+    file_path = os.path.join(current_path, filename)
+    file = open(file_path, 'w')
+    file.write(file_content)
+    file.close()
 
     # result = service.files().
 def GetFile(service,fileId):
 
     metadata = service.files().get(fileId=fileId,acknowledgeAbuse=None).execute()
-    filename = metadata['name']
+    PrintJSON(metadata)
 
-    current_path = os.getcwd()
-    file_path = os.path.join(current_path,filename)
-    file = open(file_path,'w')
-    file.write(service.files().get_media(fileId=fileId,acknowledgeAbuse=None).execute())
-    file.close()
-    print(metadata)
+    filename = metadata['name']
+    mimeType = metadata['mimeType']
+
+    media_mimeTypes =[
+        'image/jpeg',
+        'image/png',
+        'image/svg+xml'
+    ]
+
+    if mimeType in media_mimeTypes:
+        file_content  = service.files().get_media(fileId=fileId,acknowledgeAbuse=None).execute()
+        WriteToFile(filename,file_content)
+    else:
+        file_content = service.files().export(fileId=fileId,mimeType='application/pdf').execute()
+        WriteToFile(filename,file_content)
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -63,10 +145,10 @@ def get_credentials():
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
+        # if flags:
+        #     credentials = tools.run_flow(flow, store, flags)
+        # else: # Needed only for compatibility with Python 2.6
+        #     credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
 
@@ -78,11 +160,24 @@ def ListRecentFiles(service):
         print('No files found.')
     else:
         print('Files:')
-        # for item in items:
-        #     print('{0} ({1})'.format(item['name'], item['id']))
-        file = items[0]
-        fileId = file['id']
-        GetFile(service,fileId)
+        files = {}
+        count = 0
+        file_map =[]
+        for item in items:
+            print(str(count) + " "+ format(item['name'] + " "+ item['id']))
+            files[item['name']] = item['id']
+            count = count + 1
+            file_map.append(item['id'])
+
+        view_file = raw_input('Enter Corresponding No:')
+        GetFile(service,file_map[int(view_file)])
+
+def Help():
+    print("Terminal Drive Instruction Set")
+    print("Show Help : drive ")
+    print("List Recent Files : drive list")
+    print("Upload a file : drive up <filepath>")
+    print("Download File : drive down <Filename>")
 
 def main():
     """Shows basic usage of the Google Drive API.
@@ -93,20 +188,22 @@ def main():
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('drive', 'v3', http=http)
+    if len(sys.argv) < 2:
+        Help()
+    else:
+        option = sys.argv[1]
+        if option == "list":
+            ListRecentFiles(service)
+        elif option == "up":
+            file_path = sys.argv[2]
+            UplaodFile(service,file_path)
+        elif option == "down":
+            filename = sys.argv[2]
+            GetFile(service,filename)
+        else:
+            print("Not An Option")
+            Help()
+    # ListRecentFiles(service)
 
-    ListRecentFiles(service)
-    # file_path = "amit.txt"
-    # UplaodFile(service,file_path)
-
-if __name__ == '__main__':
-    main()
-
-
-
-
-
-
-
-
-
-
+# if __name__ == '__main__':
+main()
